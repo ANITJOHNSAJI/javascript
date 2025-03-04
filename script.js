70bf4a17386f034fa15d8389cf0d452d29e70215
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-03-04 16:19:49 +0530

    4/3/25

---
+++ script.js
@@ -0,0 +1,186 @@
+// console.log('welcome');
+
+// name='anit'
+// console.log('welcome',name)
+
+// console.log(x)
+// var x=10
+// var x=20
+// x=13
+// console.log(x)
+
+// let a=12
+// //let a=17
+// a=25
+// console.log(a)
+
+// const y=20
+// //console y=21
+// console.log(y)
+
+// let a=10
+// console.log(a);
+// console.log(typeof a);
+
+// let a='hii'
+//  console.log(a);
+//  console.log(typeof a);
+
+// let a=[1,23,]
+// console.log(a);
+// console.log(typeof a)
+
+// let a={name:'anit' ,age:21}
+// console.log(a);
+// console.log(typeof a)
+
+// console.log('5'+5)
+// console.log(5+5)
+
+// let a=Number("25")
+// console.log(typeof a);
+
+// let a=String(25)
+// console.log(typeof a);
+
+// let a={name:'anit',age:21}
+// console.log(a)
+// console.log(typeof a);
+
+// let x="5"+2;
+// let y="5"+true;
+// let z='5'-2;
+// console.log(x,y,z)
+
+// operaters---------------------------------------------------------
+
+// arithmetic
+// console.log(10+20);
+// console.log(10-20);
+// console.log(10*20);
+// console.log(10/20);
+// console.log(10%20);
+// console.log(10**20);
+
+// assignment
+// let a=10
+// a+=10
+// console.log(a);let a=10
+// a=10
+// b=10
+// console.log(a);
+// a='10'
+// b=10
+// console.log(a===b);
+// console.log(a!=b);
+// console.log(a!==b);
+
+// logical 
+// console.log(10==10 && 10>5)
+// console.log(10==10 || 10>5)
+
+
+
+
+// conditional statements
+
+// salary=15000
+// yearofservice=6
+// if(yearofservice>5){
+//     console.log((salary*5/100));
+// }
+// else{
+//     console.log('not eligible for bonus');
+// }
+
+// a=1
+// if(a==1){
+//     console.log('monday');
+// }
+// else if(a==2){
+//     console.log('tuesday');
+// }
+// else if(a==3){
+//     console.log('wednesday');
+// }
+// else if(a==4){
+//     console.log('thursday');
+// }
+// else if(a==5){
+//     console.log('friday');
+// }
+// else if(a==6){
+//     console.log('saturday');
+// }
+// else if(a==7){
+//     console.log('sunday');
+// }
+// else{
+//     console.log('invalid');}
+
+
+// city='delhi'
+// if(city=='delhi'){
+//     console.log(monument='red fort');
+// }
+// else if(city=='agra'){
+//     console.log(monument='taj mahal');
+// }
+// else if(city=='jaipur'){
+//     console.log(monument='jal mahal');
+// }
+// else{
+//     console.log('invalid city');
+// }
+
+// electricity_unit=90
+// if(electricity_unit<100){
+//     console.log('no charge')
+// }
+// else if(electricity_unit<=200){
+//     console.log(price=(electricity_unit-100)*5)
+// }
+// else{
+//     console.log(price=(100*5)+(electricity_unit-200)*10)
+// }
+
+
+// number=33
+// remainder=number%10
+// if(remainder%3==0)
+// {
+//     console.log('divisible by 3')
+
+// }
+// else{
+//     console.log('not divsible by 3')
+// }
+
+// price=1025000
+// if(price<1000000){
+//     console.log((price*15/100+price))
+// }
+// else if(price<500000 && price<=100000){
+//     console.log((price*10/100+price))
+// }
+// else{
+//     console.log((price*5/100+price))
+// }
+
+a=100
+b=18
+c=15
+if(a>b ){
+    if(a>c){
+    console.log('a is greater')
+    }
+
+}
+else if(b>c){
+    
+        console.log('b is greater')
+    
+}
+else{
+    console.log('c is greater')
+}
