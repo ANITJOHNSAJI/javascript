faa74d656f6311e20d92275a071b340b9da7f1b4
ANITJOHNSAJI <anitjohnsaji41@gmail.com>
2025-03-18 15:57:27 +0530

    closure,arrayfunctions

---
+++ arrayfn.js
@@ -0,0 +1,30 @@
+const a =[1,2,3,4,5,6]
+// Array.forEach(callback(element,indexedDB,arr),thisValue)
+
+// a.forEach((item)=>console.log(item))
+
+// const data=a.map((item)=>item*10)
+// console.log(data);
+
+// const data=a.filter((item)=>item%2==0)
+// console.log(data);
+
+// const data=['abc','def','ghi','asd']
+// const result=data.filter((item)=>item[0]=='a')
+// console.log(result);
+
+// const res=a.reduce((data,item)=>data+item)
+// console.log(res);
+
+// const data=a.find((item)=>item>4)
+// console.log(data);
+
+// function data(a,b,callback){
+//     const c=a+b
+//     callback(c)
+// }
+// function display(c){
+//     console.log(c);
+// }
+// data(10,2,display)
+// data(112,2,display)
---
+++ callback.js
@@ -0,0 +1,38 @@
+// const a =[1,2,3,4,5,6]
+// function data(a,b,callback){
+//     const c=a+b
+//     callback(c)
+// }
+// function display(c){
+//     console.log(c);
+// }
+// data(10,2,display)
+// data(112,2,display)
+
+// const data={
+//  fname:"manu",
+//     lname:"lal",
+//     fullname:function(){
+//         console.log(this.fname,this.lname);
+// }
+// }
+// console.log(data);  
+// data.fullname()
+
+// call
+const nameobj={
+    fname:"anil",
+    age:20
+
+}
+const data={
+    fname:"manu",
+       lname:"lal",
+       fullname:function(age){
+           console.log(this.fname,age);
+   }
+   }
+   console.log(data);  
+//    data.fullname.call(nameobj)
+const d1=data.fullname.bind(nameobj,18)
+d1()
